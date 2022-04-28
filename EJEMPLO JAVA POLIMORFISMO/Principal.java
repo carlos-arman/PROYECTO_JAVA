c7144ffcbba5a1307981414ab9b316b12f240764
carlos-arman <minerocarlos28@gmail.com>
2022-04-27 18:37:01 -0700

    CLASE PRINCIPAL MODIFICADA

--- EJEMPLO JAVA POLIMORFISMO/Principal.java
+++ EJEMPLO JAVA POLIMORFISMO/Principal.java
@@ -3,7 +3,7 @@ public class Principal{
         Vehiculo tiposVehiculos[] = new Vehiculo[4];
         tiposVehiculos[0] = new Vehiculo("Nissan", "amarillo", 2002, 4);
 
-        //POLIMORFISMO
+        //POLIMORFISMOS
         tiposVehiculos[1] = new vehiculofurgoneta("Disel", "Negro", 2020, 4);
         tiposVehiculos[2] = new vehiculoCamioneta("4x4", "Azul", 2021, 6);
         tiposVehiculos[3] = new vehiculoElectrico("nova", "Blanco", 2022, 4);
